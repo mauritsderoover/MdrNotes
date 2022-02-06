e34781b8f76e557c8fb281182ed9c71c3b7e6c01
m.deroover <maurits.de.roover@outlook.com>
2022-02-06 22:33:21 +0100

    cleaned up MainDashboardSolidData.vue

--- src/components/modules/editor/MainDashboardSolidData.vue
+++ src/components/modules/editor/MainDashboardSolidData.vue
@@ -19,24 +19,6 @@
         class="p-button-help"
         @click="deleteRootContainer"
       />
-      <!--      <Button-->
-      <!--        label="add Something to Container"-->
-      <!--        icon="pi pi-check"-->
-      <!--        class="p-button-help"-->
-      <!--        @click="addSomethingToContainer"-->
-      <!--      />-->
-      <!--      <Button-->
-      <!--        label="solidCreationExample"-->
-      <!--        icon="pi pi-check"-->
-      <!--        class="p-button-help"-->
-      <!--        @click="solidCreationExample"-->
-      <!--      />-->
-      <!--      <Button-->
-      <!--        label="solidModificationExample"-->
-      <!--        icon="pi pi-check"-->
-      <!--        class="p-button-help"-->
-      <!--        @click="solidModificationExample"-->
-      <!--      />-->
     </div>
     <div class="p-col-12">
       <Toolbar ref="toolbar" />
@@ -144,6 +126,7 @@ export default defineComponent({
       currentEditor: "",
       editor: undefined,
       activeMenuElement: undefined,
+      notebook: undefined,
     };
   },
   async beforeMount() {
@@ -240,70 +223,6 @@ export default defineComponent({
         console.log("this seems to have worked");
       });
     },
-    async createNoteBookOld() {
-      // We create a notebook
-      const resourceURL =
-        "https://mauritsderoover.solidcommunity.net/notes/someidentifier1";
-      let solidDataSet = createSolidDataset();
-      const thing = buildThing(createThing({ url: resourceURL }))
-        .addUrl(RDF.type, NOTETAKING.NoteBook)
-        .addStringNoLocale(DCTERMS.title, "NotebookName")
-        .build();
-      await saveSolidDatasetAt(resourceURL, setThing(solidDataSet, thing), {
-        fetch,
-      });
-      // we create a section and link it to a notebook
-      const sectionURL =
-        "https://mauritsderoover.solidcommunity.net/notes/sectionidentifier";
-      solidDataSet = createSolidDataset();
-      const sectionThing = buildThing(createThing({ url: sectionURL }))
-        .addUrl(RDF.type, NOTETAKING.Section)
-        .addUrl(NOTETAKING.partOfNoteBook, resourceURL)
-        .addStringNoLocale(DCTERMS.title, "A section title")
-        .build();
-      await saveSolidDatasetAt(
-        sectionURL,
-        setThing(solidDataSet, sectionThing),
-        { fetch }
-      );
-    },
-    async solidModificationExample() {
-      const resourceURL =
-        "https://mauritsderoover.solidcommunity.net/universityZ/fall2021/courses/Writing101";
-      let courseSolidDataset = await getSolidDataset(resourceURL, {
-        fetch: fetch,
-      });
-
-      let book1Thing = getThing(courseSolidDataset, `${resourceURL}#book1`);
-      if (book1Thing)
-        book1Thing = buildThing(book1Thing)
-          .addInteger("https://schema.org/numberOfPages", 30)
-          .build();
-
-      let book2Thing = getThing(courseSolidDataset, `${resourceURL}#book2`);
-      if (book2Thing)
-        book2Thing = setStringNoLocale(
-          book2Thing,
-          SCHEMA_INRUPT.name,
-          "ZYX987 of Example Poesy"
-        );
-
-      const locationThing = buildThing(createThing({ name: "location" }))
-        .addStringNoLocale(SCHEMA_INRUPT.name, "Sample Lecture Hall")
-        .addUrl(RDF.type, "https://schema.org/Place")
-        .build();
-      if (book1Thing && book2Thing) {
-        courseSolidDataset = setThing(courseSolidDataset, book1Thing);
-        courseSolidDataset = setThing(courseSolidDataset, book2Thing);
-        courseSolidDataset = setThing(courseSolidDataset, locationThing);
-      }
-
-      const savedSolidDataset = await saveSolidDatasetAt(
-        resourceURL,
-        courseSolidDataset,
-        { fetch: fetch } // fetch from authenticated Session
-      );
-    },
     async addSomethingToContainer() {
       const uri =
         "https://mauritsderoover.solidcommunity.net/testerRoot/testerYolo";
@@ -349,174 +268,15 @@ export default defineComponent({
     labelChange(event: any) {
       console.log("this has been executed", event);
     },
-    async containerExists() {
-      console.log(
-        "this is in containerExists",
-        await containerExists(
-          "https://mauritsderoover.solidcommunity.net/notes/"
-        )
-      );
-    },
-    createLabelThing(): ThingPersisted {
-      return buildThing(
-        createThing({
-          url: "https://mauritsderoover.solidcommunity.net/notes/TESTERTAB/",
-        })
-      )
-        .addStringNoLocale(DCTERMS.title, "testertitle")
-        .build();
-    },
-    deleteLabel(): void {
-      deleteSolidDataset(
-        "https://mauritsderoover.solidcommunity.net/notes/tab1/untitled%20kpocsakpo",
-        { fetch }
-      ).then(() => {
-        deleteContainerContents(
-          "https://mauritsderoover.solidcommunity.net/notes/"
-        ).then(() => {
-          deleteContainerContents(
-            "https://mauritsderoover.solidcommunity.net/notes"
-          );
-        });
+    createInitialNotes() {
+      newNoteBook("Your First Notebook").then(() => {
+        this.loadData();
       });
-    },
-    testingLabelStuff(): void {
-      getData(this.createTabUrl("TESTERTAB/").toString());
-    },
-    addLabel(): void {
-      deleteContainerContents(this.createTabUrl("TESTERTAB/").toString()).then(
-        () => {
-          createContainerAtUri(this.createTabUrl("TESTERTAB/").toString()).then(
-            (value) => {
-              console.log(
-                "this is value aftr the new tab has been created",
-                value
-              );
-              getData(this.createTabUrl("TESTERTAB/").toString()).then(
-                (value1) => {
-                  console.log("this is data", value1);
-                  if (value1) {
-                    const new_dataset = setThing(
-                      value1,
-                      this.createLabelThing()
-                    );
-                    saveSolidDatasetAt(
-                      this.createTabUrl("TESTERTAB/").toString(),
-                      new_dataset,
-                      { fetch }
-                    ).then((value2) => {
-                      console.log("this is value2", value2);
-                    });
-                  }
-                }
-              );
-            }
-          );
-        }
-      );
-
-      setTimeout(() => {
-        getData(this.createTabUrl("TESTERTAB/").toString()).then((value1) => {
-          console.log("this is data", value1);
-        });
-      }, 4000);
-    },
-    createNoteDigitalDocument(): ThingLocal {
-      return buildThing(createThing({ name: "untitled" }))
-        .addUrl(rdf.type, SCHEMA.NoteDigitalDocument)
-        .addStringEnglish(SCHEMA.abstract, "Personal notes")
-        .addUrl(SCHEMA.accountablePerson, this.$store.getters.getWebId)
-        .addUrl(SCHEMA.creator, this.$store.getters.getWebId)
-        .addDatetime(SCHEMA.dateCreated, new Date())
-        .addStringNoLocale(SCHEMA.Text, "")
-        .build();
-    },
-    createNotePage() {
-      console.log("this function has been reached");
-      let solidDataset = createSolidDataset();
-
-      solidDataset = setThing(solidDataset, this.createNoteDigitalDocument());
-
-      saveSolidDatasetAt(this.currentMenuPanelItem, solidDataset, {
-        fetch,
-      }).then(() => {
-        console.log("it has been succesfully saved");
-      });
-    },
-    async createInitialNotes() {
-      let solidDataset = createSolidDataset();
-
-      const testThing = buildThing(
-        createThing({
-          name: `nameIsBack`,
-        })
-      )
-        .addUrl(rdf.type, SCHEMA.NoteDigitalDocument)
-        .addStringEnglish(SCHEMA.abstract, "Personal notes")
-        .addUrl(SCHEMA.accountablePerson, this.$store.getters.getWebId)
-        .addUrl(SCHEMA.creator, this.$store.getters.getWebId)
-        .addDatetime(SCHEMA.dateCreated, new Date())
-        .addStringNoLocale(SCHEMA.Text, "")
-        .build();
-
-      solidDataset = setThing(solidDataset, testThing);
-
-      await saveSolidDatasetAt(
-        encodeURI(
-          `${this.$store.getters.getOrigin}/notes/tab1/untitled kpocsakpo/`
-        ),
-        solidDataset,
-        { fetch }
-      );
-
-      await this.loadData();
     },
     async loadData() {
-      // first we get the root dataset
-      const rootDataSets = await getData(
-        `${this.$store.getters.getOrigin}/notes/`
-      );
-      // from the root dataset we get the tabs
-      let tabUrls: string[] = [];
-      if (rootDataSets) {
-        const urls = getContainedResourceUrlAll(rootDataSets);
-        if (Array.isArray(urls)) {
-          tabUrls = urls;
-        }
-      }
-
+      [this.notebook, this.tabItems, this.panelMenuItems] = await loadData();
       // for every tab we get the related dataset
-      for (const tabUrl of tabUrls) {
-        const tabKey = getContainerName(tabUrl);
-        this.tabItems.push({
-          label: tabKey,
-          key: tabKey,
-          uri: new URL(tabUrl),
-        });
-        const tabDataset = await getData(tabUrl);
-        let tabMenuItemUrls: string[] = [];
-        if (tabDataset) {
-          tabMenuItemUrls = getContainedResourceUrlAll(tabDataset);
-        }
-        for (const menuItemUrl of tabMenuItemUrls) {
-          if (!this.panelMenuItems[tabKey]) {
-            this.panelMenuItems[tabKey] = [
-              new PanelItem({
-                label: getContainerName(menuItemUrl),
-                url: menuItemUrl,
-              }),
-            ];
-          } else {
-            this.panelMenuItems[tabKey].push(
-              new PanelItem({
-                label: getContainerName(menuItemUrl),
-                url: menuItemUrl,
-              })
-            );
-          }
-        }
-      }
-      this.currentTab = getContainerName(tabUrls[0]);
+      this.currentTab = this.tabItems[0].key;
       this.currentMenuPanelItem = this.panelMenuItems[this.currentTab][0].key;
       this.currentEditor = this.panelMenuItems[this.currentTab][0].key;
     },
@@ -531,6 +291,9 @@ export default defineComponent({
       }
     },
     addTab() {
+
+    },
+    addTabOld() {
       // In order to add a tab we need a the url storage container
       // we need a label
       // we need a check if the label is unique
@@ -597,7 +360,6 @@ export default defineComponent({
       });
       this.panelMenuItems[this.currentTab].push(newPanelItem);
       this.currentMenuPanelItem = newPanelItem.key;
-      this.createNotePage();
     },
     createTabUrl(name: string): URL {
       return new URL(`${this.$store.getters.getOrigin}/notes/${name}`);
@@ -620,146 +382,6 @@ export default defineComponent({
         this.editor.setContents(this.editors[this.currentEditor]);
       }
     },
-
-    // // Create and delete tabs
-    // addTabItem(tabTitel) {
-    //   // create the account
-    //   const newTab = {
-    //     label: tabTitel,
-    //     key: `tab-${this.tabItems.length}`,
-    //     newKey: `tab${this.tabItems.length}`,
-    //   };
-    //   this.tabItems.splice(this.tabItems.length - 1, 0, newTab);
-    //   this.panelMenuItems[newTab.newKey] = [
-    //     {
-    //       label: "Untitled",
-    //       icon: "pi pi-fw pi-file",
-    //       key: "main-item-1",
-    //       editor: `editor${Object.keys(this.editors).length}`,
-    //     },
-    //     {
-    //       label: "add page",
-    //       icon: "pi pi-fw pi-plus",
-    //       key: `${newTab.newKey}-main-item-0`,
-    //
-    //       command: (event) => {
-    //         this.currentItem = event.item;
-    //         this.addPanelMenuItem("untitled");
-    //       },
-    //     },
-    //   ];
-    //   this.editors[`editor${Object.keys(this.editors).length}`] = null;
-    // },
-    // removeTabItem() {},
-
-    // // Create and delete PanelMenuItems
-    // addPanelMenuItem(tabTitel) {
-    //   this.panelMenuItems[this.currentTab].splice(
-    //     this.panelMenuItems[this.currentTab].length - 1,
-    //     0,
-    //     {
-    //       label: tabTitel,
-    //       icon: "pi pi-fw pi-file",
-    //       key: `${this.currentTab}-main-item-${
-    //         this.panelMenuItems[this.currentTab].length
-    //       }`,
-    //       editor: `editor${Object.keys(this.editors).length}`,
-    //       command: (event) => {
-    //         this.currentItem = event.item;
-    //       },
-    //       items: [
-    //         {
-    //           label: "Add page",
-    //           parentKey: `${this.currentTab}-main-item-${
-    //             this.panelMenuItems[this.currentTab].length
-    //           }`,
-    //           key: `${this.currentTab}-sub-item-0`,
-    //           editor: `editor${Object.keys(this.editors).length}`,
-    //           icon: "pi pi-fw pi-file",
-    //           command: (event) => {
-    //             this.currentItem = event.item;
-    //             this.addPanelMenuSubItem("Untitled", event);
-    //           },
-    //           items: [
-    //             {
-    //               label: "another item",
-    //               key: "sub-sub-item-0",
-    //               editor: `editor${Object.keys(this.editors).length + 1}`,
-    //               items: [],
-    //             },
-    //           ],
-    //         },
-    //       ],
-    //     }
-    //   );
-    //   this.editors[`editor${Object.keys(this.editors).length}`] = null;
-    //   this.editors[`editor${Object.keys(this.editors).length + 1}`] = null;
-    // },
-    // addPanelMenuSubItem(tabTitel, event) {
-    //   console.log(
-    //     "this is tabTitel and event in addPanelMenuSubItem",
-    //     tabTitel,
-    //     event,
-    //     this.panelMenuItems,
-    //     this.currentTab
-    //   );
-    //   const mainitem = this.panelMenuItems[this.currentTab].find((mainItem) => {
-    //     return mainItem.key === event.item.parentKey;
-    //   });
-    //   mainitem.items.splice(mainitem.items.length - 1, 0, {
-    //     label: tabTitel,
-    //     parentkey: mainitem.key,
-    //     key: `sub-item-${mainitem.items.length}`,
-    //     icon: "pi pi-fw pi-file",
-    //     editor: `editor${Object.keys(this.editors).length}`,
-    //     items: [],
-    //   });
-    //   this.editors[`editor${Object.keys(this.editors).length}`] = null;
-    // },
-
-    // // Store data
-    // storeTabItems() {
-    //   saveJsonFile(JSON.stringify(this.tabItems), "/data/tab-notes.json");
-    // },
-    // saveContents() {
-    //   saveJsonFile(
-    //     JSON.stringify(this.editor.getContents()),
-    //     `${this.identifier}.json`
-    //   );
-    // },
-
-    // setInitialTabs() {
-    //   this.menuItems = {};
-    //   this.tabItems = [
-    //     {
-    //       label: "First page",
-    //       key: "tab-0",
-    //       newKey: "tab0",
-    //     },
-    //     {
-    //       icon: "pi pi-fw pi-plus",
-    //       command: (event) => {
-    //         this.addTabItem("Untitled");
-    //       },
-    //     },
-    //   ];
-    //   this.currentTab = this.tabItems[0].newKey;
-    //   this.currentMenuPanelItem = this.panelMenuItems[this.currentTab][0];
-    // },
-    // getTabItems() {
-    //   axios
-    //     .get("/data/tab-notes.json")
-    //     .then((value) => {
-    //       this.tabItems = value.data;
-    //     })
-    //     .catch(() => {
-    //       this.setInitialTabs();
-    //     });
-    // },
-    // // check if notes container exists
-    // noteContainerExist() {
-    //
-    // }
   },
 });
 </script>
